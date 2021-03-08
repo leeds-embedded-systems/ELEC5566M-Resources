593e23bbc62abb2a7a71f6446da3ff38b678ca03
Tom Carpenter <strange_orange_fish@hotmail.com>
2021-03-08 23:40:37 +0000

    Add Example of Memory Initialisation from File

---
+++ sampleCode/Unit3-2/MemoryInitialisation.v
@@ -0,0 +1,31 @@
+/*
+ * The following provides three ways of initialising a Verilog
+ * memory:
+ *
+ *  1. Using an initial block
+ *  2. Reading from a text file with $readmemb/h
+ *  3. Initialising from structured memory initialisation file by attribute.
+ *
+ */
+
+//Initialise using an initial block
+//  - This should work with all synthesis tools
+reg [7:0] ram [0:15];
+initial begin
+    ram[0] = 8'd100;
+    ram[1] = 8'd123;
+    ram[2] = 8'd0;
+    ...
+end
+
+//Initialize with the Verilog HDL $readmemb or $readmemh
+//  - This is supported by most synthesis tools
+reg [7:0] ram [0:15];
+initial begin
+    $readmemb("ram.txt", ram);
+end
+
+//Initialize with a structured file using Verilog Attribute
+//  - This is Quartus specific.
+(* ram_init_file = "ram.mif" *) reg [7:0] ram [0:15];
+
